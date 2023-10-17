55f36a22381416debc821c062270323598ba6a2a
Rahmad Adi Pratama <OhMyVenyx@proton.me>
2023-10-17 09:19:36 +0700

    ysl: Bringup Orangefox 12.1
    Signed-off-by: Rahmad Adi Pratama <OhMyVenyx@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_ysl.mk
+	$(LOCAL_DIR)/twrp_ysl.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_ysl-user \
+    twrp_ysl-userdebug \
+    twrp_ysl-eng
--- twrp_ysl.mk
+++ twrp_ysl.mk
@@ -9,16 +9,22 @@ PRODUCT_RELEASE_NAME := ysl
 
 $(call inherit-product, build/target/product/embedded.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from twrp common
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore=msm8953 \
-    ro.treble.enabled=true
+    ro.treble.enabled=true \
+    ro.bootimage.build.date.utc=1514797200 \
+    ro.build.date.utc=1514797200
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.bootimage.build.date.utc \
+    ro.build.date.utc
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ysl
-PRODUCT_NAME := omni_ysl
+PRODUCT_NAME := twrp_ysl
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi S2
 PRODUCT_MANUFACTURER := Xiaomi
